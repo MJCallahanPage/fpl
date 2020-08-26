b268d9022e49566c8bf4479cb6cf830f86e342c6
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2020-08-26 21:49:35 +0100

    [AddBootstrapLibrary] Refactor to use messages file and MessagesBaseController

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,13 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import play.api.i18n.Messages.implicitMessagesProviderToMessages
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: MessagesControllerComponents) extends MessagesBaseController {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index() = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok(views.html.index())
   }
 }
